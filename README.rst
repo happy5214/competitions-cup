fb5bcb4d4b1f1d113326655508de170c1348b0d4
Alexander Jones <happy5214@gmail.com>
2015-10-06 04:30:50 -0500

    Initial commit
    Partially cloned from competitions-scheduler.

---
+++ README.rst
@@ -0,0 +1,12 @@
+Generic cup competitions
+========================
+
+The goal of this package is to provide high-quality models of knockout cup
+competitions.
+
+The first version will include basic four-team and eight-team
+single-elimination cups.
+
+.. image:: https://travis-ci.org/happy5214/competitions-cup.svg?branch=master
+    :alt: Build status
+    :target: https://travis-ci.org/happy5214/competitions-cup
---
+++ setup.py
@@ -0,0 +1,43 @@
+"""A setuptools based setup module."""
+
+from setuptools import setup, find_packages
+
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='competitions-cup',
+    version='0.1a1',
+
+    description='Generic cup competitions',
+    long_description=long_description,
+
+    url='https://github.com/happy5214/competitions-cup',
+
+    author='Alexander Jones',
+    author_email='happy5214@gmail.com',
+
+    license='LGPLv3+',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+
+        'License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)',
+
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+    keywords='competitions cups knockout elimination',
+
+    packages=find_packages(exclude=['docs', 'tests*']),
+
+    namespace_packages=['competitions'],
+
+    test_suite='tests',
+)
